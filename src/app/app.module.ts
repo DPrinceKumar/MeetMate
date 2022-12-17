f65994ba1acf9d442c2d165577292f623570aaf8
DPrinceKumar <prince2kumar4147@gmail.com>
2022-12-17 19:25:13 +0530

    nav bar of meetmate page with responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavBarComponent],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {
+  faHeart,
+  faUserCircle,
+  faCompass,
+  faPlusSquare,
+} from '@fortawesome/free-regular-svg-icons';
+import {
+  faRightFromBracket,
+  faMagnifyingGlass,
+  faHouse,
+  faCamera
+} from '@fortawesome/free-solid-svg-icons';
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css'],
+})
+export class NavBarComponent {
+  faHeart = faHeart;
+  faCompass = faCompass;
+  faUserCircle = faUserCircle;
+  faRightFromBracket = faRightFromBracket;
+  faMagnifyingGlass = faMagnifyingGlass;
+  faHouse = faHouse;
+  faPlusSquare = faPlusSquare;
+  faCamera = faCamera;
+}
