71da275577e1a7e207d0d9ac720c8eb875aebc7b
DPrinceKumar <prince2kumar4147@gmail.com>
2022-12-20 14:40:33 +0530

    Feed card completed with some fix of design and rightProfile section created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { StatusBarComponent } from './status-bar/status-bar.component';
+import { FeedCardComponent } from './feed-card/feed-card.component';
+import { ProfileSwitchComponent } from './profile-switch/profile-switch.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, StatusBarComponent],
+  declarations: [AppComponent, NavBarComponent, StatusBarComponent, FeedCardComponent, ProfileSwitchComponent],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/profile-switch/profile-switch.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-switch',
+  templateUrl: './profile-switch.component.html',
+  styleUrls: ['./profile-switch.component.css']
+})
+export class ProfileSwitchComponent {
+
+}
