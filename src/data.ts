22ac6e185e8b7a9e349ecf90018f565212ffb48a
DPrinceKumar <prince2kumar4147@gmail.com>
2022-12-22 16:35:30 +0530

    dummy data

---
+++ src/data.ts
@@ -0,0 +1,17 @@
+const data = {
+  usernames: '@abtdprincekumar',
+  fullName: 'Prince Kumar',
+  email: 'abtdprincekumar@gmail.com',
+  profileImageUrl:
+    'https://s.yimg.com/ny/api/res/1.2/_9cxvN3sUtbvORHBCgQPzg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://s.yimg.com/os/creatr-uploaded-images/2022-11/41dd6c70-5ac9-11ed-9b1f-d433e456385f',
+  profileImageAlt: 'User profile image',
+  postFeed:
+    'https://m.belfasttelegraph.co.uk/sport/football/world-cup/4fe23/42217500.ece/AUTOCROP/w620/WCup%20Netherlands%20Argentina%20Soccer',
+  postFeedAlt: 'feed-',
+  comments: ' ❤️ 👌 ',
+  commentCount: 12,
+  postDays: 30,
+  statusImg:
+    'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
+  statusbarAlt:"status-"
+};
