56c2839411f922bc0315ecac63a8704ca34d42da
DPrinceKumar <prince2kumar4147@gmail.com>
2022-12-22 11:52:09 +0530

    minor style and content of html changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { StatusBarComponent } from './status-bar/status-bar.component';
 import { FeedCardComponent } from './feed-card/feed-card.component';
 import { ProfileSwitchComponent } from './profile-switch/profile-switch.component';
+import { SuggestionsForYouComponent } from './suggestions-for-you/suggestions-for-you.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, StatusBarComponent, FeedCardComponent, ProfileSwitchComponent],
+  declarations: [AppComponent, NavBarComponent, StatusBarComponent, FeedCardComponent, ProfileSwitchComponent, SuggestionsForYouComponent],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
