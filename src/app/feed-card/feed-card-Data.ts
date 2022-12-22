96baffd6061f83fb58b38fe53ed1548939ddcc22
DPrinceKumar <prince2kumar4147@gmail.com>
2022-12-22 12:25:56 +0530

    dynamic feed loading done

---
+++ src/app/feed-card/feed-card-Data.ts
@@ -0,0 +1,10 @@
+export interface FeedCardDataList{
+    profilePicture: string;
+    userName:string;
+    feedImageUrl:string;
+    feedImageAlt:string;
+    comments:string;
+    commentCount:number;
+    daysAgoCount:number;
+
+}
--- src/app/feed-card/feed-card.component.ts
+++ src/app/feed-card/feed-card.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FeedCardDataList } from './feed-card-Data';
 
 @Component({
   selector: 'app-feed-card',
   templateUrl: './feed-card.component.html',
   styleUrls: ['./feed-card.component.css']
 })
-export class FeedCardComponent {
+export class FeedCardComponent implements OnInit {
+  constructor(){}
+  feedCardDataList:FeedCardDataList[]=[];
+  ngOnInit(): void {
+    this.feedCardDataList = [
+      {
+        profilePicture:"https://s.yimg.com/ny/api/res/1.2/_9cxvN3sUtbvORHBCgQPzg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://s.yimg.com/os/creatr-uploaded-images/2022-11/41dd6c70-5ac9-11ed-9b1f-d433e456385f",
+        userName: 'Prince Kumar',
+        feedImageUrl:'https://m.belfasttelegraph.co.uk/sport/football/world-cup/4fe23/42217500.ece/AUTOCROP/w620/WCup%20Netherlands%20Argentina%20Soccer',
+        feedImageAlt:"feed-Prince Kumar",
+        comments:"😁🙌",
+        commentCount:30,
+        daysAgoCount:12
+      },
+      {
+        profilePicture:"https://s.yimg.com/ny/api/res/1.2/_9cxvN3sUtbvORHBCgQPzg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://s.yimg.com/os/creatr-uploaded-images/2022-11/41dd6c70-5ac9-11ed-9b1f-d433e456385f",
+        userName: 'Pankaj Kuamr',
+        feedImageUrl:
+          'https://m.belfasttelegraph.co.uk/sport/football/world-cup/4fe23/42217500.ece/AUTOCROP/w620/WCup%20Netherlands%20Argentina%20Soccer',
+          feedImageAlt:"feed-Pankaj Kuamr",
+        comments:"😁🙌",
+        commentCount:30,
+        daysAgoCount:12
+      },
+      {
+        profilePicture:"https://s.yimg.com/ny/api/res/1.2/_9cxvN3sUtbvORHBCgQPzg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://s.yimg.com/os/creatr-uploaded-images/2022-11/41dd6c70-5ac9-11ed-9b1f-d433e456385f",
+        userName: 'Anuttar Jain',
+        feedImageUrl:
+          'https://m.belfasttelegraph.co.uk/sport/football/world-cup/4fe23/42217500.ece/AUTOCROP/w620/WCup%20Netherlands%20Argentina%20Soccer',
+          feedImageAlt:"feed-Anuttar Jain",
+        comments:"😁🙌",
+        commentCount:30,
+        daysAgoCount:12
+      },
+      {
+        profilePicture:"https://s.yimg.com/ny/api/res/1.2/_9cxvN3sUtbvORHBCgQPzg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://s.yimg.com/os/creatr-uploaded-images/2022-11/41dd6c70-5ac9-11ed-9b1f-d433e456385f",
+        userName: 'anurag Tomar',
+        feedImageUrl:
+          'https://m.belfasttelegraph.co.uk/sport/football/world-cup/4fe23/42217500.ece/AUTOCROP/w620/WCup%20Netherlands%20Argentina%20Soccer',
+          feedImageAlt:"feed-anurag Tomar",
+        comments:"😁🙌",
+        commentCount:30,
+        daysAgoCount:12
+      },
+      {
+        profilePicture:"https://s.yimg.com/ny/api/res/1.2/_9cxvN3sUtbvORHBCgQPzg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://s.yimg.com/os/creatr-uploaded-images/2022-11/41dd6c70-5ac9-11ed-9b1f-d433e456385f",
+        userName: 'Kishan Kumar',
+        feedImageUrl:
+          'https://m.belfasttelegraph.co.uk/sport/football/world-cup/4fe23/42217500.ece/AUTOCROP/w620/WCup%20Netherlands%20Argentina%20Soccer',
+          feedImageAlt:"feed-Kishan Kumar",
+        comments:"😁🙌",
+        commentCount:30,
+        daysAgoCount:12
+      },
+      {
+        profilePicture:"https://s.yimg.com/ny/api/res/1.2/_9cxvN3sUtbvORHBCgQPzg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://s.yimg.com/os/creatr-uploaded-images/2022-11/41dd6c70-5ac9-11ed-9b1f-d433e456385f",
+        userName: 'jhon lal',
+        feedImageUrl:
+          'https://m.belfasttelegraph.co.uk/sport/football/world-cup/4fe23/42217500.ece/AUTOCROP/w620/WCup%20Netherlands%20Argentina%20Soccer',
+          feedImageAlt:"feed-jhon lal",
+        comments:"😁🙌",
+        commentCount:30,
+        daysAgoCount:12
+      },
+    ];
+  }
 
 }
