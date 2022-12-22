cda791c7108bfcc986b0d7a27ac9e26c6b1d40fb
DPrinceKumar <prince2kumar4147@gmail.com>
2022-12-22 11:51:06 +0530

    suggestions area completed

---
+++ src/app/suggestions-for-you/suggestions-for-you.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suggestions-for-you',
+  templateUrl: './suggestions-for-you.component.html',
+  styleUrls: ['./suggestions-for-you.component.css']
+})
+export class SuggestionsForYouComponent {
+
+}
