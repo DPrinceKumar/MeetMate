0331f78c81066c69856f4c33f7a7e612b43bb146
DPrinceKumar <prince2kumar4147@gmail.com>
2022-12-22 16:39:00 +0530

    meetmatedata interface for global data transfer to all component

--- src/app/status-bar/status-bar.component.ts
+++ src/app/status-bar/status-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { statusDataList } from '../statusData';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { MeetMateDataList } from '../MeetMateData';
+import { StatusDataList } from './statusData';
 
 @Component({
   selector: 'app-status-bar',
@@ -7,84 +8,9 @@ import { statusDataList } from '../statusData';
   styleUrls: ['./status-bar.component.css'],
 })
 export class StatusBarComponent implements OnInit {
-  statusDataList: statusDataList[] = [];
-
   constructor() {}
   ngOnInit(): void {
-    this.statusDataList = [
-      {
-        imageSrc:
-          'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
-        userName: 'Prince kumar',
-        imageAlt: 'Prince kumar',
-      },
-      {
-        imageSrc:
-          'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
-        userName: 'Rahul kumar',
-        imageAlt: 'Prince kumar',
-      },
-      {
-        imageSrc:
-          'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
-        userName: 'khan Ali',
-        imageAlt: 'Prince kumar',
-      },
-      {
-        imageSrc:
-          'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
-        userName: 'jenyfer irne',
-        imageAlt: 'Prince kumar',
-      },
-      {
-        imageSrc:
-          'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
-        userName: 'pakia tak',
-        imageAlt: 'Prince kumar',
-      },
-      {
-        imageSrc:
-          'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
-        userName: 'Anuttar jain',
-        imageAlt: 'Prince kumar',
-      },
-      {
-        imageSrc:
-          'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
-        userName: 'anurag tomar',
-        imageAlt: 'Prince kumar',
-      },
-      {
-        imageSrc:
-          'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
-        userName: 'Prince kumar',
-        imageAlt: 'Prince kumar',
-      },
-      {
-        imageSrc:
-          'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
-        userName: 'Prince kumar',
-        imageAlt: 'Prince kumar',
-      },
-      {
-        imageSrc:
-          'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
-        userName: 'Prince kumar',
-        imageAlt: 'Prince kumar',
-      },
-      {
-        imageSrc:
-          'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
-        userName: 'Prince kumar',
-        imageAlt: 'Prince kumar',
-      },
-      {
-        imageSrc:
-          'https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg',
-        userName: 'Prince kumar',
-        imageAlt: 'Prince kumar',
-      },
-    ];
-    
   }
+
+  @Input() status: MeetMateDataList[];
 }
