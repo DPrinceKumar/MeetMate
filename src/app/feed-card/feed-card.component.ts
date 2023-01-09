312fbbb086be22f2ee70ea8166eef3fcecee9539
DPrinceKumar <prince2kumar4147@gmail.com>
2023-01-09 10:32:55 +0530

    like btn,more menu modal working

--- src/app/feed-card/feed-card.component.ts
+++ src/app/feed-card/feed-card.component.ts
@@ -11,13 +11,15 @@ import { StatusService } from '../services/status/status.service';
   selector: 'app-feed-card',
   templateUrl: './feed-card.component.html',
   styleUrls: ['./feed-card.component.css'],
-  // changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FeedCardComponent implements OnInit {
   status: any;
   userInfo: any;
   Readmore = false;
+  displayStyle = 'none';
   constructor() {}
+
   // constructor(private meetMate: StatusService) {
   //   console.log('meetMateData', meetMate);
   //   meetMate.status().subscribe((data) => {
@@ -36,15 +38,70 @@ export class FeedCardComponent implements OnInit {
   //   });
   // }
 
-  // dayCount(numOfDays: number): Number {
-  //   //d1.getDate()-new Date(1666965991000).getDate()
-  //   //console.log(d1-new Date(1666965991000));
-  //   //Math.round(5343652822 /(3600000*24))
+  /**
+   * converting unix days int to date
+   * @param numOfDays
+   * @returns date
+   */
+  dayCount(numOfDays: number): Number {
+    //d1.getDate()-new Date(1666965991000).getDate()
+    //console.log(d1-new Date(1666965991000));
+    //Math.round(5343652822 /(3600000*24))
 
-  //   return new Date(numOfDays).getDate();
+    return new Date(numOfDays).getDate();
+  }
+
+  /**
+   * comment slicing
+   */
+
+  // comment: string;
+  // slicedComment: string;
+  // moreLess(_id: string, comment: String){
+  //   this.feed.map((curVal) => {
+  //     if (curVal._id === _id) {
+  //       return this.comment;
+  //     } else {
+  //       return (this.slicedComment = comment.slice(0, 50));
+  //     }
+  // });
   // }
+
   @Input() feed: MeetMateDataList[];
+
   ngOnInit(): void {
     console.log('oninixt,', this.feed);
+  }
+
+  /**
+   * OPEN MORE POPUP
+   */
+  openMorePopup(): void {
+    this.displayStyle = 'block';
+    // alert('Popup is open');
+  }
+  closePopup() {
+    this.displayStyle = 'none';
+  }
+
+  /**
+   * like dislike
+   */
+
+  displayLikeOutLine: string = 'block';
+  displayLikesolid: string = 'none';
+  flag: boolean = true;
+
+  likeDisLike() {
+    if (this.flag == true) {
+      this.displayLikeOutLine = 'none';
+      this.displayLikesolid = 'block';
+      this.flag = false;
+    } else {
+      this.displayLikeOutLine = 'block';
+      this.displayLikesolid = 'none';
+      this.flag = true;
+      console.log('owbject');
+    }
   }
 }
